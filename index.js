94a023ce0a08065cf12c4a0872655311951f1ec1
Emmanuel Vasquez <emmanuelvs2011@gmail.com>
2021-09-07 17:07:11 -0600

    fixed cors

--- index.js
+++ index.js
@@ -14,5 +14,10 @@ app.use(express.urlencoded({
   extended: true
 }));
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use('/api', router);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
